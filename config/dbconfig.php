1849ddb0a29bdd8aca7bea6e9f9bcc2fbe6f85d3
Sérgio Moura <smouracalmon@gmail.com>
2020-06-08 12:20:02 -0300

    Estruturada classe de conexão com banco de dados

---
+++ config/dbconfig.php
@@ -0,0 +1,7 @@
+<?php
+
+    define("DB_NAME","fake_instagram");
+    define("DB_USER","sergio");
+    define("DB_PASS","vaiplaneta");
+    define("DB_HOST","localhost");
+    define("DB_PORT", 3306);
--- insert_data.php
+++ insert_data.php
@@ -1,18 +1,15 @@
 <?php
 
-    /* Connect to a MySQL database using driver invocation */
-    $dsn = 'mysql:dbname=fake_instagram;host=localhost;port=3306';
-    $user = 'sergio';
-    $password = 'vaiplaneta';
+    include_once(__DIR__.'/classes/DB.php');
     
     try {
-        $db = new PDO($dsn, $user, $password);
+        $db = new DB();
     } catch (PDOException $e) {
         echo 'Connection failed: ' . $e->getMessage();
     }
 
     echo("executando...");
-    $sql = 'INSERTlasdal INTO usuarios (nome, email, senha, username, foto) VALUES ("Sérgio","sergio@teste","123123","sergio",null)';
+    $sql = 'INSERT INTO usuarios (nome, email, senha, username, foto) VALUES ("Sérgio","sergio@teste","123123","sergio",null)';
     if($db->query($sql)){
        echo("executado!");
     } else {
